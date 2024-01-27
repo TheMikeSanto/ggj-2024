1ee13dde4ae6c03d592ef4b69b6c350989b69d7d
Mike Santo <mike.j.santo@gmail.com>
2024-01-27 15:51:29 -0600

    unbroke

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,11 +1,11 @@
 import { Scene } from 'phaser';
 
-import { AudienceMemberEntity } from '../entities/audience-member';
+import { AudienceMember, PeopleType } from '../entities/audience-member';
 
 export class Game extends Scene {
   private stage;
 
-  private audience: AudienceMemberEntity[];
+  private audience: AudienceMember[];
 
   constructor() {
     super({
@@ -16,18 +16,21 @@ export class Game extends Scene {
   create(): void {
     this.audience =  [...new Array(38)].flatMap((value, index) => {
       return [
-        AudienceMemberEntity.create(this, 'blue-1', 15 + index * 20, 50),
-        AudienceMemberEntity.create(this, 'blue-2', 15 + index * 20, 100),
-        AudienceMemberEntity.create(this, 'green-1', 15 + index * 20, 150),
-        AudienceMemberEntity.create(this, 'purple-1', 15 + index * 20, 200),
-        AudienceMemberEntity.create(this, 'purple-2', 15 + index * 20, 250),
-        AudienceMemberEntity.create(this, 'red-1', 15 + index * 20, 300),
-        AudienceMemberEntity.create(this, 'red-2', 15 + index * 20, 350),
-        AudienceMemberEntity.create(this, 'yellow-1', 15 + index * 20, 400),
-        AudienceMemberEntity.create(this, 'yellow-2', 15 + index * 20, 450),
+        AudienceMember.create(this, PeopleType.blue1, 15 + index * 20, 50),
+        AudienceMember.create(this, PeopleType.blue2, 15 + index * 20, 100),
+        AudienceMember.create(this, PeopleType.green1, 15 + index * 20, 150),
+        AudienceMember.create(this, PeopleType.purple1, 15 + index * 20, 200),
+        AudienceMember.create(this, PeopleType.purple2, 15 + index * 20, 250),
+        AudienceMember.create(this, PeopleType.red1, 15 + index * 20, 300),
+        AudienceMember.create(this, PeopleType.red2, 15 + index * 20, 350),
+        AudienceMember.create(this, PeopleType.yellow1, 15 + index * 20, 400),
+        AudienceMember.create(this, PeopleType.yellow2, 15 + index * 20, 450),
       ];
     });
-    this.audience = [...this.audience, AudienceMemberEntity.create(this, 'green-1', 700, 350, 1)];
+    this.audience = [
+      ...this.audience,
+      AudienceMember.create(this, PeopleType.green1, 700, 350, 1),
+    ];
     this.stage = this.add.image(300, 840, 'stage');
 
     this.add.image(210, 790, 'button-red').setScale(0.15);
