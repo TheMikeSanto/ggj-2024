b1081192c870a6e6c220a1e8ba1952f9aace26cf
Nick Veys <nick@veys.xyz>
2024-01-27 16:46:16 -0600

    Shake it up

--- src/entities/button.ts
+++ src/entities/button.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
 import * as Phaser from 'phaser';
+import ShakePosition from 'phaser3-rex-plugins/plugins/behaviors/shake/ShakePosition';
 
 export enum ButtonColor {
   Red = 'red',
@@ -10,35 +11,17 @@ export enum ButtonColor {
 
 export class Button extends Phaser.GameObjects.Sprite {
 
-  private static readonly DefaultAngle = 0;
-
-  private static readonly DefaultScale = 0.20;
-
-  private static readonly PressedAngle = 2;
-
-  private static readonly PressedScale = 0.19;
+  private readonly shake: ShakePosition;
 
   constructor(scene: Phaser.Scene, color, x, y) {
     super(scene, x, y, `button-${color}`, 0);
     scene.add.existing(this);
     this.setInteractive();
-
-    this.on('pointerdown', () => this.setPressedState());
-    this.on('pointerup', () => this.setDefaultState());
-    this.on('pointerout', () => this.setDefaultState());
-    this.setDefaultState()
+    this.setScale(0.2);
+    this.shake = new ShakePosition(this, {});
+    this.on('pointerdown', () => this.shake.shake());
   }
 
   public update(time: number, delta: number): void {
-  }
-
-  private setDefaultState() {
-    this.setAngle(Button.DefaultAngle);
-    this.setScale(Button.DefaultScale);
-  }
-
-  private setPressedState() {
-    this.setAngle(Button.PressedAngle);
-    this.setScale(Button.PressedScale);
   }
 }
